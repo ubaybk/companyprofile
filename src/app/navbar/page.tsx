efa1cc6e62355b130fa2c3f4133c670a0e13c8f7
ubaybk <bayu05061992@gmail.com>
2024-03-15 22:13:23 +0700

    tambahIcon

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
         <div className=" bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="img/Logo icon.png" className="h-8" alt="Flowbite Logo" />
+                    <img src="img/Logo Icon.png" className="h-8" alt="rumahLogo" />
                     <span className="font-extrabold self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Rumah_Ubay</span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
