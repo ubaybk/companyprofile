8e20ec5d906c1476ec5caa4a5ad3c0d4a7eab939
ubaybk <bayu05061992@gmail.com>
2024-03-19 22:33:27 +0700

    newFiturPotrait

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -93,14 +93,14 @@ export default function Navbar() {
 
                     <ScrollLink
                         activeClass="active"
-                        to="services"
+                        to="album"
                         spy={true}
                         smooth={true}
                         offset={-70}
                         duration={500}
                     >
                         <li>
-                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                            <a href="album" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Album</a>
                         </li>
                     </ScrollLink>
 
