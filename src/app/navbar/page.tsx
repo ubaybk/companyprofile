3fbe81789d0374ba9a6b4f3dfa94773a9bb6ca42
ubaybk <bayu05061992@gmail.com>
2024-03-22 10:52:10 +0700

    buil navbar

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 import { useState } from "react"
 import { Link as ScrollLink } from 'react-scroll';
-import Link from 'next/link'
-import Home from "../page";
-import HeroSection from "../heroSection/page";
+// import Link from 'next/link'
+// import Home from "../page";
+// import HeroSection from "../heroSection/page";
 export default function Navbar() {
 
     const handleWhatsAppClick = () => {
@@ -21,10 +21,10 @@ export default function Navbar() {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="img/Logo Icon.png" className="h-8" alt="rumahLogo" />
-                    <span className="font-extrabold self-center text-2xl whitespace-nowrap dark:text-white">Rumah_Ubay</span>
+                    <span className="font-extrabold self-center text-[18px] whitespace-nowrap dark:text-white">Rumah_Ubay</span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button onClick={handleWhatsAppClick} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Order Sekarang</button>
+                    <button onClick={handleWhatsAppClick} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[10px] px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Order Sekarang</button>
                     <button onClick={() => setShow(!show)} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="false" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
